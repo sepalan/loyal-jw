a8337aacde9ad2af47ef63d5a8a7e969c84cabe0
sepalan <sepalan.serge@sepalan.net>
2018-03-15 09:46:47 +0200

    task_4_5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -13,3 +13,11 @@
 '''
 
 VLANS = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+#VLANS = set(VLANS)
+#VLANS = list(VLANS)
+#VLANS.sort()
+#print(VLANS)
+
+VLANS = (list(set(VLANS)))
+VLANS.sort()
+print(VLANS)
